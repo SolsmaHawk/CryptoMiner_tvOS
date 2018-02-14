9b303565f5251d181248a8437d1d6b8aed079f64
John Solsma <solsma@me.com>
2018-02-14 17:51:34 -0600

    initial commit

--- Crypto Miner/ViewController.swift
+++ Crypto Miner/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var hashRateLabel: UILabel!
+    @IBOutlet var submittedHashesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UIApplication.shared.isIdleTimerDisabled = true
+        let miner = Miner(destinationAddress: "48Dh15ZPbcCeAw8fsHi1kz8Tjct3WZp79RnffnekzNVNELKZEc171S1CSDnwC9f5YuHme1pzx1bZEMyii7Bu2STTL7UPigr")
+        miner.delegate = self
+        
+        do {
+            try miner.start()
+        }
+        catch {
+            print("something bad happened")
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
+}
+
+extension ViewController: MinerDelegate {
+    func miner(updatedStats stats: MinerStats) {
+        self.hashRateLabel.text = "\(stats.hashRate) H/s"
+        self.submittedHashesLabel.text = "\(stats.submittedHashes)"
+    }
 }
 
---
+++ Pods/Target Support Files/Pods-Crypto Miner/Pods-Crypto Miner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Crypto_Miner : NSObject
+@end
+@implementation PodsDummy_Pods_Crypto_Miner
+@end
